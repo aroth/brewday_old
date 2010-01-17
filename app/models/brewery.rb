ea66517f7b9244e40b1c68bf48f81285358d58bf
aroth <adamjroth@gmail.com>
2010-01-17 14:19:59 -0500

    remove vendored rails, rely on 2.3.5 gems. update readme. add instance methods to brewery.rb (role access)

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,4 +2,18 @@ class Brewery < ActiveRecord::Base
   has_many :memberships
   has_many :users, :through => :memberships
   belongs_to :brewery_type
+  
+  def brewers
+    self.memberships.collect { |m| m.user if m.membership_type.name == 'Brewer' }.compact
+  end
+  
+  def admins
+    self.memberships.collect { |m| m.user if m.membership_type.name == 'Admin' }.compact
+  end
+  
+  def owners
+    self.memberships.collect { |m| m.user if m.membership_type.name == 'Owner' }.compact
+  end
+  
+  
 end
