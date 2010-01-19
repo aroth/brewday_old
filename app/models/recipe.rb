1c027f156dc5718a2163386fc532e09dfd1f8b2d
aroth <adamjroth@gmail.com>
2010-01-19 16:35:28 -0500

    fix login redirection; added recipes_controller + updated seed data (login as test1/password to see everything in action)

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,6 @@ class Recipe < ActiveRecord::Base
   #belongs_to :user
   belongs_to :brewery
   belongs_to :beer_style
+  has_many :batches
   #belongs_to :beer_style_category, :through => :beer_style
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => "users"
+  
   map.resources :users
+  
   map.resources :breweries
+  map.resources :recipes
+  
   map.resource :user_session
   map.root :controller => "user_sessions", :action => "new"
 end
